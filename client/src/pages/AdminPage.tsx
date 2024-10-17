bb5473969c659aa134f8e8a26ea552238caa805c
ifrachid <144709963+ifrachid@users.noreply.github.com>
2024-10-17 22:03:52 +0100

    Delete client directory

--- client/src/pages/AdminPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const AdminPage = () => {
-  //table of all users and a dropdown to change the realm access to whatever the admind desires
-  return <div>AdminPage</div>;
-};
-
-export default AdminPage;
