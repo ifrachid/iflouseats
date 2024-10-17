3375a922931f214bf9f6f3eecdf731bc2ccdc755
ifrachid <144709963+ifrachid@users.noreply.github.com>
2024-10-17 22:04:08 +0100

    Delete server directory

--- server/src/routes/ContactRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import ContactController from "../controllers/ContactController";
-
-const router = express.Router();
-
-router.post("/", ContactController.createTicket);
-
-export default router;
